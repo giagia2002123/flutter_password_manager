0a300daefc9335477f19cafd21045912046311a7
cvn_andy_ngo <andy.ngo@contemi.com.vn>
2021-12-07 16:50:26 +0700

    update code 1.3
    update code 1.3

--- lib/passwordpage.dart
+++ lib/passwordpage.dart
@@ -14,9 +14,58 @@ class _PassworPageState extends State<PassworPage> {
       backgroundColor: dark,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          ListTile(
-            leading: FaIcon(FontAwesomeIcons.facebook),
+          Container(
+            decoration: BoxDecoration(
+              color: deeppurple,
+            ),
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: ListTile(
+              leading: FaIcon(
+                FontAwesomeIcons.facebook,
+                size: 40.0,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Facebook",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: Text(
+                "Password",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20.0),
+            decoration: BoxDecoration(
+              color: deeppurple,
+            ),
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: ListTile(
+              leading: FaIcon(
+                FontAwesomeIcons.instagram,
+                size: 40.0,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Instagram",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: Text(
+                "Password",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ),
         ],
       ),
